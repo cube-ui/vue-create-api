e853bb238514696bc13c35efc7c69d491b2bdadf
Kntt <kntt9968@gmail.com>
2019-02-22 01:06:22 +0800

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,34 @@
+import Vue, { Component, PluginFunction, ComponentOptions, VNode, CreateElement } from 'vue'
+
+declare module 'vue/types/vue' {
+  export interface Vue {
+    createAPI: (Component: Component, events?: string[], single?: boolean) => Api
+  }
+}
+
+export interface ApiOption {
+  componentPrefix?: string
+  apiPrefix?: string
+}
+
+export interface renderFunction {
+  (createElement: CreateElement): VNode
+}
+
+export interface Api {
+  before: (config: object,renderFn: renderFunction, single: boolean) => void,
+  create: (config: object,renderFn: renderFunction, single: boolean) => Component
+}
+
+export interface instantiateComponent {
+  (Vue: Vue, Component: Component, data: object, renderFn: renderFunction, options: ComponentOptions<Vue>): Component
+}
+
+export interface CreateAPI {
+  install: PluginFunction<ApiOption>
+  instantiateComponent: instantiateComponent
+  version: string
+}
+
+declare const CreateAPI: CreateAPI
+export default CreateAPI
